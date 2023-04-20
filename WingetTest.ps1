a028fa90e37aabcde74d72260f6354480def48e7
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2023-04-20 13:44:39 -0500

    Update WingetTest.ps1

--- WingetTest.ps1
+++ WingetTest.ps1
@@ -1,46 +1,34 @@
 $progressPreference = 'silentlyContinue'
 
-# Download PsExec
-Write-Information "Downloading PsExec..."
-$psexecUrl = "https://download.sysinternals.com/files/PSTools.zip"
-$psexecZipFile = "PSTools.zip"
-
-Invoke-WebRequest -Uri $psexecUrl -OutFile $psexecZipFile
-
-# Extract PsExec
-Expand-Archive -Path $psexecZipFile -DestinationPath ".\PSTools\"
-$psexecPath = ".\PSTools\PsExec64.exe"
-
 # Download Winget
 Write-Information "Downloading Winget..."
 $wingetMsixBundleUrl = "https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 $wingetMsixBundle = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 Invoke-WebRequest -Uri $wingetMsixBundleUrl -OutFile $wingetMsixBundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
 
-# Install Winget using PsExec
-Write-Information "Installing Winget using PsExec..."
-& $psexecPath -s powershell.exe -ExecutionPolicy Bypass -Command {
-    Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage $wingetMsixBundle
-}
+# Add Winget as a provisioned app
+Write-Information "Installing Winget as a provisioned app..."
+Add-AppxProvisionedPackage -Path $wingetMsixBundle -Online
 
-# Run the following steps using PsExec as the System user
+# Get the path to AppInstallerCLI.exe
+$app = Get-AppxPackage -AllUsers -Name "Microsoft.DesktopAppInstaller"
+$wingetCliPath = Join-Path -Path $app.InstallLocation -ChildPath "AppInstallerCLI.exe"
+
+# Install apps using winget
 $commands = @(
-    "winget install -e --id 7zip.7zip --accept-source-agreements --scope machine",
-    "winget install -e --id VideoLAN.VLC --accept-source-agreements --scope machine",
-    "winget install -e --id Zoom.Zoom --accept-source-agreements --scope machine",
-    "winget install -e --id Zoom.ZoomOutlookPlugin --accept-source-agreements --scope machine",
-    "winget install -e --id Google.Chrome --accept-source-agreements --scope machine",
-    "winget install -e --id Mozilla.Firefox --accept-source-agreements --scope machine"
+    "install --id 7zip.7zip --exact --source winget --scope machine",
+    "install --id VideoLAN.VLC --exact --source winget --scope machine",
+    "install --id Zoom.Zoom --exact --source winget --scope machine",
+    "install --id Zoom.ZoomOutlookPlugin --exact --source winget --scope machine",
+    "install --id Google.Chrome --exact --source winget --scope machine",
+    "install --id Mozilla.Firefox --exact --source winget --scope machine"
 )
 
 foreach ($command in $commands) {
-    Write-Information "Running command with PsExec: $command"
-    & $psexecPath -s powershell.exe -ExecutionPolicy Bypass -Command $command
+    Write-Information "Running winget command: $command"
+    & $wingetCliPath $command
 }
 
 # Install .NET 3.5 from Add/Remove Windows Features
 Write-Information "Installing .NET 3.5..."
 Enable-WindowsOptionalFeature -Online -FeatureName NetFx3
-Write-Information ".NET 3.5 installation complete."
