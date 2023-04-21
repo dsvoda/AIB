3b36de4a6bd30c168767d8b435806fbf45771ab3
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2023-04-21 07:10:27 -0500

    Update WingetTest.ps1

--- WingetTest.ps1
+++ WingetTest.ps1
@@ -1,15 +1,3 @@
-$progressPreference = 'silentlyContinue'
-
-# Download Winget
-Write-Information "Downloading Winget..."
-$wingetMsixBundleUrl = "https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-$wingetMsixBundle = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-Invoke-WebRequest -Uri $wingetMsixBundleUrl -OutFile $wingetMsixBundle
-
-# Add Winget as a provisioned app
-Write-Information "Installing Winget as a provisioned app..."
-Add-AppxProvisionedPackage -PackagePath $wingetMsixBundle -Online -SkipLicense
-
 # Get the path to AppInstallerCLI.exe
 $app = Get-AppxPackage -AllUsers -Name "Microsoft.DesktopAppInstaller"
 $wingetCliPath = Join-Path -Path $app.InstallLocation -ChildPath "AppInstallerCLI.exe"
@@ -28,7 +16,3 @@ foreach ($command in $commands) {
     Write-Information "Running winget command: $command"
     Start-Process -FilePath $wingetCliPath -ArgumentList $command -Wait
 }
-
-# Install .NET 3.5 from Add/Remove Windows Features
-Write-Information "Installing .NET 3.5..."
-Enable-WindowsOptionalFeature -Online -FeatureName NetFx3
