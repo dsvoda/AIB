b59fa545216f4ab210eca8065e42098bcd5f621d
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2023-04-23 05:52:25 -0500

    Create install-apps.ps1

---
+++ install-apps.ps1
@@ -0,0 +1,41 @@
+# install_apps.ps1
+
+$appsDir = "C:\Gobi\apps"
+$logFile = "C:\Gobi\AIB_ApplicationInstall_$(Get-Date -Format 'yyyyMMdd').log"
+
+
+# Function to write to the log file
+function Write-Log {
+    Param($message)
+    Write-Output "$(Get-Date -Format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
+}
+
+# Function to install an application
+function Install-App {
+    Param($appName, $appScript)
+
+    Write-Host "AIB Customization: Installing $appName"
+    try {
+        . "$appsDir\$appScript"
+        Write-Log "$appName installed successfully"
+        Write-Host "$appName installed successfully"
+    }
+    catch {
+        $ErrorMessage = $_.Exception.Message
+        Write-Log "Error installing ${appName}: ${ErrorMessage}"
+        Write-Host "Error installing ${appName}: ${ErrorMessage}"
+    }
+}
+
+# List of applications to install
+$applications = @(
+    @{
+        Name = "Microsoft Desktop App Installer";
+        Script = "install-wingetapps.ps1";
+    }
+)
+
+# Install each application
+foreach ($application in $applications) {
+    Install-App -appName $application.Name -appScript $application.Script
+}
