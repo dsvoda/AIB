8411b0bc6e9da354c7d41a74df44be10b8a68f7f
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2023-04-23 19:06:35 -0500

    Update install-apps.ps1

--- install-apps.ps1
+++ install-apps.ps1
@@ -16,9 +16,7 @@ function Install-App {
 
     Write-Host "AIB Customization: Installing $appName"
     try {
-        $scriptPath = Join-Path -Path $PSScriptRoot -ChildPath $appScript
-        $command = "powershell.exe -ExecutionPolicy Bypass -File `"$scriptPath`""
-        Invoke-Expression $command
+        & $appScript
         Write-Log "$appName installed successfully"
         Write-Host "$appName installed successfully"
     }
